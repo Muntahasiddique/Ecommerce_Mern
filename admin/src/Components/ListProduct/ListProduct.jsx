d12999e570563459e695eef56b15d3d1d1ae0d7f
muntaha <muntahamirza890@gmail.com>
2025-08-31 01:47:35 +0500

    Added admin panel Product list

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,11 +1,46 @@
-import './ListProduct.css'
-
+import { useEffect, useState } from "react";
+import "./ListProduct.css";
+import cross_icon from '../../assets/cross_icon.png'
 const ListProduct = () => {
-  return (
-    <div className='ListProduct' >
-      
-    </div>
-  )
-}
+  const [allproducts, setallproducts] = useState([]);
 
-export default ListProduct
+  // to fetch data and save in state
+  const fetchinfo = async () => {
+    await fetch("http://localhost:4000/allproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setallproducts(data);
+      });
+  };
+  useEffect(()=>{
+    fetchinfo()
+  },[])
+  return (
+    <div className="ListProduct">
+      <h1>All products list</h1>
+      <div className="listproduct-formate-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product,index)=>{
+           return    <div  key={index}       className="listProduct-formate-main listProduct-Formate ">
+               <img src={product.image} alt="" className="listproduct-product-icon" />
+               <p>{product.name}</p>
+               <p>${product.old_price}</p>
+                <p>${product.new_price}</p>
+                 <p>{product.category}</p>
+                 <img src={cross_icon} className="listproduct-removeicon" alt="" />
+           </div>
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ListProduct;
