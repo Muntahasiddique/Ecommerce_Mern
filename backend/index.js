0820960417ae6302e8eb5d21eb3d1e0d1c772b07
muntaha <muntahamirza890@gmail.com>
2025-08-31 03:17:47 +0500

    Added the backend for login signup with awt token

--- backend/index.js
+++ backend/index.js
@@ -126,6 +126,84 @@ app.get('/allproducts' , async(req , res)=>{
     res.send(products)
 })
 
+//user Schema 
+const Users = mongoose.model('Users',{
+  name:{
+    type:String,
+  },
+   email:{
+      type:String,
+      unique:true,
+     
+    },
+     password:{
+        type:String
+      },
+      cartData:{
+        type:Object,
+      },
+      date:{
+        type:Date,
+        default:Date.now,
+      }
+})
+
+//End point for Registering user
+app.post('/signup',async (req,res)=>{
+  let check = await Users.findOne({email:req.body.email})
+  if (check) {
+    return res.status(400).json({success:false,errors:"exiting User Found with same email id"})
+  }
+  let cart ={}
+  for (let i = 0; i < 300; i++) {
+  cart[i] =0;
+    
+  }
+  const user = new Users({
+    name:req.body.name,
+    email:req.body.email,
+     password:req.body.password,
+      cartData:req.body.cart,
+  })
+  await user.save();
+
+  //jwt authentaication
+  const data = {
+    user:{
+      id : user.id
+    }
+  }
+  const token = jwt.sign(data,'secret_ecom');
+  res.json({success:true,token})
+})
+
+//endpoint for user login
+app.post('/login' , async (req , res)=>{
+let user = await Users.findOne({
+  email:req.body.email
+})
+if(user){
+  const passCompare = req.body.password === user.password;
+  if(passCompare){
+    const data = {
+      user:{
+        id:user.id
+      }
+    }
+    const token = jwt.sign(data,'secret_ecom')
+    res.json({success:true,token})
+  }
+  else{
+    res.json({success:false,errors:"Wrong password"})
+  }
+}
+else{
+  res.json({
+    success:false,errors:"Wrong email"
+  })
+}
+})
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("server running on " + port);
